b690028253794bbe5df62f29a000419c9350a72d
John Coote <john_coote@rocketmail.com>
2022-08-28 17:34:16 +1000

    added the endpoint

--- app/controllers/hook_receiver_controller.rb
+++ app/controllers/hook_receiver_controller.rb
@@ -2,5 +2,10 @@ require 'rpi_gpio'
 
 class HookReceiverController < ApplicationController
 
-  
+  def animal
+    # receiving webhooks from animal crossing
+    puts request.inspect
+    puts request.to_h
+    puts request.to_json
+  end
 end
